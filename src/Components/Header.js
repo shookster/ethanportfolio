749790dabfb2ce0f1a86975672395f93a8319ec9
Ethan Shook <eshook89@gmail.com>
2021-03-04 00:35:34 -0600

    deleted contact section

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -49,11 +49,6 @@ class Header extends Component {
                 Works
               </a>
             </li>
-            <li>
-              <a className="smoothscroll" href="#contact">
-                Contact
-              </a>
-            </li>
           </ul>
         </nav>
 
